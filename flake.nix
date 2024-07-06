ff8c2633eb011e3a6acc21f62a0fb90327256592
Blake Johnson <blakekjohnson4@gmail.com>
2024-07-05 20:38:49 -0400

    initial git setup

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Basic configuration flake";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs?ref=nixos-unstable;
+    home-manager.url = github:nix-community/home-manager;
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.middlec = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = inputs;
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
