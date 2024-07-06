100bc554f9a633f8a8486c1b3efabe35eb06688e
Blake Johnson <blakekjohnson4@gmail.com>
2024-07-05 21:13:08 -0400

    enable autocomplete and history for zsh

--- home.nix
+++ home.nix
@@ -21,6 +21,13 @@
 
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
+
+    history = {
+      size = 10000;
+      path = "${config.xdg.dataHome}/zsh/history";
+    };
+
     oh-my-zsh = {
       enable = true;
       plugins = [];
