4054cba23cb27050a89bd750c612776813c59a9e
Blake Johnson <blakekjohnson4@gmail.com>
2024-07-07 16:07:00 -0400

    add xmonad config

--- home.nix
+++ home.nix
@@ -17,6 +17,50 @@
     TERMINAL = "alacritty";
   };
 
+  xsession.windowManager.xmonad = {
+    enable = true;
+    enableContribAndExtras = true;
+    config = pkgs.writeText "xmonad.hs" ''
+      import XMonad
+
+      import XMonad.Hooks.DynamicLog
+      import XMonad.Hooks.EwmhDesktops
+      import XMonad.Hooks.StatusBar
+      import XMonad.Hooks.StatusBar.PP
+
+      import XMonad.Util.EZConfig
+
+      import XMonad.Layout.ThreeColumns
+
+      import Graphics.X11.ExtraTypes.XF86
+
+      main :: IO ()
+      main = xmonad
+           . ewmhFullscreen
+           . ewmh
+           $ blakeConfig
+
+      blakeLayout = threeCol ||| Full ||| tiled 
+        where
+          threeCol = ThreeColMid nmaster delta ratio
+          tiled = Tall nmaster delta ratio
+          nmaster = 1
+          ratio = 1/2
+          delta = 3/100
+
+      xmonadCommand = "xmonad --recompile; killall xmobar; xmonad --restart; xmobar &"
+
+      blakeConfig = def
+          { borderWidth = 5
+          , layoutHook = blakeLayout
+          }
+        `additionalKeysP`
+          [("M-q", spawn xmonadCommand)
+          , ("M-S-<Return>", spawn "alacritty")
+          ]
+    '';
+  };
+  
   programs.alacritty = {
     enable = true;
     settings = {
