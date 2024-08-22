5bc26e64644be99575926289c6d291fd6b3f2fb5
Blake Johnson <blake.k.johnson.4@gmail.com>
2024-08-22 03:30:39 -0400

    add completion to neovim

--- users/blakej/neovim.nix
+++ users/blakej/neovim.nix
@@ -47,6 +47,45 @@
           require'lspconfig'.marksman.setup{}
         '';
       }
+      cmp-nvim-lsp
+      {
+        plugin = nvim-cmp;
+        type = "lua";
+        config = ''
+          local cmp = require'cmp'
+          cmp.setup{
+            snippet = {
+              expand = function(args)
+                vim.snippet.expand(args.body)
+              end,
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<CR>'] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+              },
+	      ['<Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+		  cmp.select_next_item()
+		else
+		  fallback()
+		end
+	      end, { 'i', 's' }),
+	      ['<S-Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+		  cmp.select_prev_item()
+		else
+		  fallback()
+		end
+	      end, { 'i', 's' }),
+            }),
+            sources = {
+              { name = 'nvim_lsp' },
+            }
+          }
+        '';
+      }
     ];
     extraLuaConfig = ''
       require'nvim-treesitter.configs'.setup {
