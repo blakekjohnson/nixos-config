70c33dc97c5b94de06915929b9a0d276f5600fd5
Blake Johnson <blake.k.johnson.4@gmail.com>
2024-08-22 04:18:21 -0400

    cleanup neovim config (#2)
    * add markdown neovim support (lsp+ts)
    
    * add completion to neovim
    
    * remove neovim mouse support
    
    * add telescope

--- users/blakej/neovim.nix
+++ users/blakej/neovim.nix
@@ -1,4 +1,9 @@
 { config, pkgs, ...}: {
+  home.packages = with pkgs; [
+    pkgs.marksman
+    pkgs.ripgrep
+  ];
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
@@ -33,8 +38,57 @@
       }
       (nvim-treesitter.withPlugins (p: [
         p.markdown
+        p.markdown-inline
         p.nix
       ]))
+      {
+        plugin = nvim-lspconfig;
+        type = "lua";
+        config = ''
+          require'lspconfig'.marksman.setup{}
+        '';
+      }
+      cmp-nvim-lsp
+      {
+        plugin = nvim-cmp;
+        type = "lua";
+        config = ''
+          local cmp = require'cmp'
+          cmp.setup{
+            snippet = {
+              expand = function(args)
+                vim.snippet.expand(args.body)
+              end,
+            },
+            mapping = cmp.mapping.preset.insert({
+              ['<C-Space>'] = cmp.mapping.complete(),
+              ['<CR>'] = cmp.mapping.confirm {
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+              },
+	      ['<Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+		  cmp.select_next_item()
+		else
+		  fallback()
+		end
+	      end, { 'i', 's' }),
+	      ['<S-Tab>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+		  cmp.select_prev_item()
+		else
+		  fallback()
+		end
+	      end, { 'i', 's' }),
+            }),
+            sources = {
+              { name = 'nvim_lsp' },
+            }
+          }
+        '';
+      }
+      plenary-nvim
+      telescope-nvim
     ];
     extraLuaConfig = ''
       require'nvim-treesitter.configs'.setup {
@@ -45,13 +99,22 @@
           enable = true,
         },
       }
+
+      vim.opt.conceallevel = 2
     '';
     extraConfig = ''
       :set number
       :set shiftwidth=2 smarttab
       :set expandtab
       :set tabstop=8 softtabstop=0
+      :set mouse=
       nnoremap <C-L><C-L> :set invrelativenumber<CR>
+
+      nnoremap <leader>ff <cmd>Telescope find_files<CR>
+      nnoremap <leader>fb <cmd>Telescope buffers<CR>
+      nnoremap <leader>fg <cmd>Telescope live_grep<CR>
+      nnoremap <leader>fh <cmd>Telescope help_tags<CR>
+      nnoremap <leader>ft <cmd>Telescope treesitter<CR>
     '';
   };
 }
